a7fd962323ba6b87414e315744f138044666c1f0
NSBSPL <m.reza.rezaei72@gmail.com>
2022-10-10 14:38:42 -0400

    [V2.0.0.3] [Dev] add simple classifier

--- 204_D4_phoneme_prediction.py
+++ 204_D4_phoneme_prediction.py
@@ -1,8 +1,14 @@
 from data_utilities import *
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from neural_utils import  calDesignMatrix_V2
+import matplotlib.pyplot as plt
+import torch.optim as optim
+import torch.nn as nn
+import torch
+from deep_models import SimpleClassifier, get_trial_data
 import pickle
+
+
 patient_id = 'DM1008'
 datasets_add = './Datasets/'
 data_add = datasets_add + patient_id + '/' + 'Preprocessed_data/'
@@ -13,27 +19,31 @@ with open(data_add+'language_model_data.pkl', 'rb') as openfile:
 pwtwt1, phoneme_duration_df, phones_NgramModel, phones_code_dic = language_data
 
 ''' logistic regression model as discriminative model'''
-
-model_LR = LogisticRegression(multi_class='multinomial', solver='lbfgs')
-
-
-hk = 1
-
-for trial in range(1,2):
-    file_name = data_add + 'trials/trial_' + str(trial) + '.pkl'
-    with open(file_name, "rb") as open_file:
+file_name = data_add + 'trials/trial_' + str(1) + '.pkl'
+with open(file_name, "rb") as open_file:
         data_list_trial = pickle.load(open_file)
-    X = np.swapaxes(data_list_trial[0], 2, 0)
-    X = X.reshape([X.shape[0], -1])
-    y = data_list_trial[1][data_list_trial[1].columns[data_list_trial[1].columns.str.contains("id_onehot")]].to_numpy()
-    XDesign = calDesignMatrix_V2(X,hk+1).reshape([X.shape[0], -1])
-    model_LR.fit(XDesign, y)
-    y_hat=model_LR.predict(XDesign)
-    # print('acc=%f-percent\n'%(accuracy_score(y,y_hat)))
-    y_hat_prob = model_LR.predict_proba(XDesign)
 
-    ''' DDD filtering'''
+h_k = 0
+Input_size = (data_list_trial[0].shape[0]*data_list_trial[0].shape[1])*(h_k+1)
+output_size = data_list_trial[1][data_list_trial[1].columns[data_list_trial[1].columns.str.contains("id_onehot")]].shape[-1]
+hidden_dim = 100
+model = SimpleClassifier(Input_size,output_size, hidden_dim )
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+criterion = nn.CrossEntropyLoss()
+LOSS=[]
+''' training'''
+for epochs in range(20):
+    for trial in range(1, 10):
+        XDesign, y_tr = get_trial_data(data_add, trial, h_k)
+        y_hat = model.forward(XDesign)
+        y_hat = torch.nn.functional.softmax(y_hat, dim=1)
+        loss = criterion(y_hat, y_tr)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    LOSS.append(loss.detach().numpy())
 
-    p_wXT = np.zeros((y_hat_prob.shape[0],y_hat_prob.shape[1]))
-    y_hat_DDD=np.argmax(p_wXT, axis=-1)
-    print('trial = %d, hk =%d----prediction_acc=%f\n'%(trial, hk,accuracy_score(y,y_hat)))
+
+
+plt.figure()
+plt.plot(LOSS)
---
+++ deep_models.py
@@ -0,0 +1,60 @@
+import torch
+import torch.nn as nn
+from neural_utils import  calDesignMatrix_V2
+import torch.nn.functional as F
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import pickle
+
+def get_trial_data(data_add,trial, h_k):
+    '''
+    get a batch of data and [re[are it for the model
+    :param data_add: data address
+    :param trial: trial id
+    :param h_k: length of history
+    :return:
+    '''
+    scaler = StandardScaler()
+    file_name = data_add + 'trials/trial_' + str(trial) + '.pkl'
+    with open(file_name, "rb") as open_file:
+        data_list_trial = pickle.load(open_file)
+
+    X_tr = np.swapaxes(data_list_trial[0], 2, 0)
+    X_tr = X_tr.reshape([X_tr.shape[0], -1])
+    X_tr = scaler.fit_transform(X_tr)
+    y_tr = data_list_trial[1][data_list_trial[1].columns[data_list_trial[1].columns.str.contains("id_onehot")]].to_numpy()
+
+    XDesign = calDesignMatrix_V2(X_tr, h_k + 1).reshape([X_tr.shape[0], -1])
+    return torch.tensor(XDesign, dtype=torch.float32),  torch.tensor(y_tr, dtype=torch.float32)
+
+class SimpleClassifier(nn.Module):
+    """
+    simple classifier
+    """
+    def __init__(self, Input_size, output_size, hidden_dim):
+        super(SimpleClassifier,  self).__init__()
+        # 1 input image channel, 6 output channels, 5x5 square convolution
+        # kernel
+        self.Input_size = Input_size
+        self.output_size = output_size
+        self.hidden_dim = hidden_dim
+
+        self.fcin = nn.Linear(self.Input_size, self.hidden_dim)
+        self.fch = nn.Linear(self.hidden_dim, self.hidden_dim)
+        self.fout =nn.Linear(self.hidden_dim, self.output_size)
+
+    def forward(self, x):
+        # Max pooling over a (2, 2) window
+        x =  F.relu(self.fcin(x))
+        # If the size is a square, you can specify with a single number
+        x =  F.relu(self.fch(x))
+        x = self.fout(x)
+        # x = F.softmax(x, dim=1)
+        return x
+
+
+    def accuracy(self, out, labels):
+        _, pred = torch.max(out, dim=1)
+        return torch.sum(pred == labels).item()
+
+
